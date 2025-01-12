fcf26c828941d36a8a693226ee5b023ac48b51f5
al8n <scygliu1@gmail.com>
2025-01-12 18:09:17 +0800

    Update sanitizer.sh

--- ci/sanitizer.sh
+++ ci/sanitizer.sh
@@ -5,17 +5,18 @@ set -ex
 export ASAN_OPTIONS="detect_odr_violation=0 detect_leaks=0"
 
 # Run address sanitizer
-RUSTFLAGS="-Z sanitizer=address --cfg all_tests" \
-cargo test --tests --target x86_64-unknown-linux-gnu --all-features
+RUSTFLAGS="--cfg all_skl_tests -Z sanitizer=address" \
+cargo test --lib --all-features --target x86_64-unknown-linux-gnu
 
 # # Run leak sanitizer
-# RUSTFLAGS="-Z sanitizer=leak --cfg all_tests" \
-# cargo test --tests --target x86_64-unknown-linux-gnu --all-features
+# RUSTFLAGS="--cfg all_skl_tests -Zsanitizer=leak" \
+# cargo test -Zbuild-std --release --tests --target x86_64-unknown-linux-gnu --features memmap
 
 # Run memory sanitizer
-RUSTFLAGS="-Z sanitizer=memory --cfg all_tests" \
-cargo test --tests --target x86_64-unknown-linux-gnu --all-features
+RUSTFLAGS="--cfg all_skl_tests -Zsanitizer=memory -Zsanitizer-memory-track-origins" \
+RUSTDOCFLAGS="-Zsanitizer=memory -Zsanitizer-memory-track-origins" \
+cargo test -Zbuild-std --release --tests --target x86_64-unknown-linux-gnu --all-features 
 
 # Run thread sanitizer
-RUSTFLAGS="-Z sanitizer=thread --cfg all_tests" \
-cargo -Zbuild-std test --tests --target x86_64-unknown-linux-gnu --all-features
+RUSTFLAGS="--cfg all_skl_tests -Z sanitizer=thread" \
+cargo -Zbuild-std test --lib --target x86_64-unknown-linux-gnu --all-features
