8d45d5182b67162c44887ed05b92c479abf20f41
al8n <scygliu1@gmail.com>
2025-05-03 18:25:22 +0800

    Update private_ip_addrs.rs

--- src/private_ip_addrs.rs
+++ src/private_ip_addrs.rs
@@ -29,7 +29,13 @@ use super::{os, IfNet, Ifv4Net, Ifv6Net};
 ///
 /// [RFC 6890]: https://tools.ietf.org/html/rfc6890
 pub fn private_ipv4_addrs() -> io::Result<SmallVec<Ifv4Net>> {
-  os::interface_ipv4_addresses(0, private_ip_filter)
+  cfg_if::cfg_if! {
+    if #[cfg(windows)] {
+      os::interface_ipv4_addresses(None, private_ip_filter)
+    } else {
+      os::interface_ipv4_addresses(0, private_ip_filter)
+    }
+  }
 }
 
 /// Returns all IPv6 addresses that are part of [RFC
@@ -51,7 +57,13 @@ pub fn private_ipv4_addrs() -> io::Result<SmallVec<Ifv4Net>> {
 ///
 /// [RFC 6890]: https://tools.ietf.org/html/rfc6890
 pub fn private_ipv6_addrs() -> io::Result<SmallVec<Ifv6Net>> {
-  os::interface_ipv6_addresses(0, private_ip_filter)
+  cfg_if::cfg_if! {
+    if #[cfg(windows)] {
+      os::interface_ipv6_addresses(None, private_ip_filter)
+    } else {
+      os::interface_ipv6_addresses(0, private_ip_filter)
+    }
+  }
 }
 
 /// Returns all IP addresses that are part of [RFC
